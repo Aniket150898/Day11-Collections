08e8ce623698e1b2ab0bd48729708da5a8887e13
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-04-08 21:32:04 +0530

    [Aniket]- Add Program Welcome message

---
+++ CollectionProblems/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to collection practice Problem ");
+        }
+    }
+}
